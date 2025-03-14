5f02e8d196998576f1f2e3195665f57cd7b2cfad
Syarique Syahrizal <145297721+syarique31@users.noreply.github.com>
2025-03-14 18:59:48 -0400

    Add files via upload

---
+++ hackapp_LATEST(N)/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'home.dart';
+import 'matching_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Roommate Finder',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/matching': (context) => const MatchingPage(),
+      },
+    );
+  }
+}
